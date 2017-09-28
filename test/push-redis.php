0138287d5409e6bc9a97a35bdd999b99cd966840
ethan <nicesoft333@qq.com>
2017-09-28 22:13:24 +0800

    later

--- test/push-redis.php
+++ test/push-redis.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace PFinal\Queue\Test {
-
-    use PFinal\Queue\Job;
-
-    require_once __DIR__ . '/../vendor/autoload.php';
-
-    date_default_timezone_set('PRC');
-
-    $queue = new \PFinal\Queue\Driver\Redis();
-
-    //$id = $queue->push('Foo@bar', ['id' => 1], 'default', 10);
-    //var_dump($id);
-
-    $job = $queue->pop();
-    if ($job) {
-        var_dump($job->getJobId());
-        $job->delete();
-    } else {
-        var_dump($job);
-    }
-
-}
+//namespace PFinal\Queue\Test {
+//
+//    use PFinal\Queue\Job;
+//
+//    require_once __DIR__ . '/../vendor/autoload.php';
+//
+//    date_default_timezone_set('PRC');
+//
+//    $queue = new \PFinal\Queue\Driver\Redis();
+//
+//    //$id = $queue->push('Foo@bar', ['id' => 1], 'default', 10);
+//    //var_dump($id);
+//
+//    $job = $queue->pop();
+//    if ($job) {
+//        var_dump($job->getJobId());
+//        $job->delete();
+//    } else {
+//        var_dump($job);
+//    }
+//
+//}
